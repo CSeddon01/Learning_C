e6272621c91be0e9807bbd1e92240963dafe725c
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-03-22 13:15:26 -0500

    Update hello.c
    Added conditionals

--- hello.c
+++ hello.c
@@ -30,4 +30,17 @@ int main(void)
     
     printf("%li\n", x + y);
 }
+//Adding conditionals
+if (x < y)
+{
+    printf("x is less than y\n");
+}
+else if (x > y)
+{
+    printf("x is not less than y\n");
+}
+else 
+{
+    printf("x is equal to y\n");
+}
 
