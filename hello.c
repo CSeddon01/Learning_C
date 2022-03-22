a91af250398f99ff2c8f6aea4f492281899228a9
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-03-22 12:55:31 -0500

    Update hello.c
    code calculator.c
    make calculator

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     printf("Hello, world!\n");
 }
+
+// basic calculator
+
+int main(void)
+{
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    printf("%i\n", x + y);
+}
+
